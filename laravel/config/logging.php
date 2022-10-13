cff98386045c766d1b6fd622904950876212486f
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2022-10-13 17:06:47 +0200

    Configurando laravel

--- laravel/config/logging.php
+++ laravel/config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['file', 'errorlog'],
             'ignore_exceptions' => false,
         ],
 
@@ -102,6 +102,12 @@ return [
         'syslog' => [
             'driver' => 'syslog',
             'level' => env('LOG_LEVEL', 'debug'),
+        ],
+
+	'file' => [
+           'driver' => 'single',
+           'path' => storage_path('logs/laravel.log'),
+           'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'errorlog' => [
