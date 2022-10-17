c2e933992d8688bfa2e144eb64a40793c32e529e
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2022-10-17 17:00:01 +0200

    Configurando databases

--- laravel/config/admin.php
+++ laravel/config/admin.php
@@ -2,6 +2,6 @@
 
 return [
    'name'      => env('ADMIN_NAME', 'admin'),
-   'email'     => env('ADMIN_EMAIL', 'admin@localhost'),
-   'password'  => env('ADMIN_PASSWORD', 'admin1234'),
+   'email'     => env('ADMIN_EMAIL', '2daw10.equip10@fp.insjoaquimmir.cat'),
+   'password'  => env('ADMIN_PASSWORD', 'Pr0j3ct3!'),
 ];
