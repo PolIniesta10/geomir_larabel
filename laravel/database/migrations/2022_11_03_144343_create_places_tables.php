c70713b571ac04133211e63df6bf7df33362cebf
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2022-11-03 17:05:44 +0100

    Migration places

--- laravel/database/migrations/2022_11_03_144343_create_places_tables.php
+++ laravel/database/migrations/2022_11_03_144343_create_places_tables.php
@@ -16,11 +16,11 @@ return new class extends Migration
         Schema::create('places', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->varchar('description', 255);
-            $table->unsignedBigInteger('file_id', 20);
+            $table->string('description');
+            $table->unsignedBigInteger('file_id');
             $table->float('latitude',8,2);
             $table->float('longitude',8,2);
-            $table->unsignedBigInteger('author_id', 20);
+            $table->unsignedBigInteger('author_id');
             $table->foreign('author_id')->references('id')->on('users');
             $table->timestamps();
         });
