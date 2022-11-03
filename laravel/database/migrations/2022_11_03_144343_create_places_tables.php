22925cdbef9683f07bcb6a77b22599d360c2eff8
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2022-11-03 16:47:54 +0100

    Migrations

---
+++ laravel/database/migrations/2022_11_03_144343_create_places_tables.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->varchar('description', 255);
+            $table->unsignedBigInteger('file_id', 20);
+            $table->float('latitude',8,2);
+            $table->float('longitude',8,2);
+            $table->unsignedBigInteger('author_id', 20);
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+};
