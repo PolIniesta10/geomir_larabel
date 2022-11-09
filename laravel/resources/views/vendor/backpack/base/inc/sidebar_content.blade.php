8b7717d8b58448f96d9c580711e43a7f5614c902
osmepa <osmepa@fp.insjoaquimmir.cat>
2022-11-09 19:31:59 +0100

    icona canviat visibility

--- laravel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ laravel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,4 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="las la-user"></i> Users</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('visibility') }}"><i class="nav-icon la la-question"></i> Visibilities</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('visibility') }}"><i class="las la-eye"></i></i> Visibilities</a></li>
