ef432d3f2cbe7387da1473e415630d64d9c37dac
osmepa <osmepa@fp.insjoaquimmir.cat>
2022-11-15 16:34:39 +0100

    usercrud arreglat

--- laravel/app/Http/Controllers/Admin/UserCrudController.php
+++ laravel/app/Http/Controllers/Admin/UserCrudController.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+
 use Backpack\PermissionManager\app\Http\Controllers\UserCrudController as PM_UserCrudController;
-use App\Http\Requests\UserRequest;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
 /**
@@ -13,24 +12,14 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
  */
 class UserCrudController extends PM_UserCrudController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
-
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
      * 
      * @return void
      */
     public function setup()
-    { 
-
+    {
         parent::setup();
-        CRUD::setModel(\App\Models\User::class);
-        CRUD::setRoute(config('backpack.base.route_prefix') . '/user');
-        CRUD::setEntityNameStrings('user', 'users');
     }
 
     /**
