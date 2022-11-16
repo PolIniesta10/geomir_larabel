bc09412934624b3f15c338172d163855703f5149
osmepa <osmepa@fp.insjoaquimmir.cat>
2022-11-16 19:17:04 +0100

    css modificat

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="prueba">
-    <div class="prueba2">
-        <div class="prueba3"><h1>{{ __('Login') }}</h1>   
+<div class="master">
+    <div class="cajaloginresgister">
+        <div class="cajaizq"><h1>{{ __('Login') }}</h1>   
         </div>
-        <div class="prueba4">
-            <div class="prueba8">
+        <div class="cajader">
+            <div class="loginregis">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
-                    <div class="prueba6">
+                    <div class="camposform">
                         <h2>Ingresar a tu cuenta</h2>
                     </div>
-                    <div class="prueba6">
+                    <div class="camposform">
                         <div class="confirmemailpass">
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
@@ -24,7 +24,7 @@
                         </div>
                     </div>
 
-                    <div class="prueba6">
+                    <div class="camposform">
                         <div class="confirmemailpass">
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
@@ -36,8 +36,8 @@
                         </div>
                     </div>
 
-                    <div class="prueba7">
-                        <div class="prueba9">
+                    <div class="confirmarform">
+                        <div class="remember">
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -46,7 +46,7 @@
                                 </label>
                             </div>
                         </div>
-                        <div class="prueba10">
+                        <div class="enviarform">
                             <button type="submit" class="btn btn-primary">
                                 {{ __('Login') }}
                             </button>
