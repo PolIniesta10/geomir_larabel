8ef6565f9ccb4869afb825c59f933701f7183212
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2022-11-22 17:39:02 +0100

    Diseñando...

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.box-app')
+@extends('layouts.app')
 @vite(['resources/js/script.js'])
 <!-- @section('box-title')
     {{ __('Dashboard') }}
@@ -17,6 +17,159 @@
         <li><a href="{{ url('/places') }}">{{ __('Places') }}</a></li>
     </ul>
 @endsection -->
+
+@section('content')
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<div class="container">
+   <div class="row justify-content-center">
+       <div class="col-md-8">
+            
+            <div class="my-card">
+                <div class="my-card-header">
+                    <img src="https://cdn-icons-png.flaticon.com/512/74/74472.png" alt="user">
+                    <div class="sub-card-header">
+                        <div>
+                            <h5>Usuario</h5>
+                        </div>
+                        <div>
+                            <p>Ubicación</p>
+                        </div>
+                    </div>
+                    <div class="tiempo-header">
+
+                        <p>15h</p>
+
+                    </div>
+                </div>
+
+                <div class="home-container">
+                    <div class="home-topics">
+                        <img src="https://www.willysplan.com/wp-content/uploads/2020/11/mirador-de-la-figuerassa.jpg" alt="Mirador Chulisimo">
+                    </div>
+                </div>
+
+                <div class="text-topics">
+                    <div>
+                        <button><i class="fa-regular fa-heart" onclick="changeIcon(this)"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-comment"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-floppy-disk"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-share-from-square"></i></button>
+                    </div>
+                </div>
+
+                <div class="text-topics">
+                    <a href="#">Ver todos los comentarios</a>
+                </div>
+            </div>
+
+            <div class="my-card">
+                <div class="my-card-header">
+                    <img src="https://cdn-icons-png.flaticon.com/512/74/74472.png" alt="user">
+                    <div class="sub-card-header">
+                        <div>
+                            <h5>Usuario</h5>
+                        </div>
+                        <div>
+                            <p>Ubicación</p>
+                        </div>
+                    </div>
+                    <div class="tiempo-header">
+
+                        <p>15h</p>
+
+                    </div>
+                </div>
+
+                <div class="home-container">
+                    <div class="home-topics">
+                        
+                        <img src="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg" alt="Mirador Chulisimo">
+                        
+                        
+                    </div>
+                </div>
+
+                <div class="text-topics">
+                    <div>
+                        <button><i class="fa-regular fa-heart" onclick="changeIcon(this)"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-comment"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-floppy-disk"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-share-from-square"></i></button>
+                    </div>
+                </div>
+
+                <div class="text-topics">
+                    <a href="#">Ver todos los comentarios</a>
+                </div>
+            </div>
+
+            <div class="my-card">
+                <div class="my-card-header">
+                    <img src="https://cdn-icons-png.flaticon.com/512/74/74472.png" alt="user">
+                    <div class="sub-card-header">
+                        <div>
+                            <h5>Usuario</h5>
+                        </div>
+                        <div>
+                            <p>Ubicación</p>
+                        </div>
+                    </div>
+                    <div class="tiempo-header">
+
+                        <p>15h</p>
+
+                    </div>
+                </div>
+
+                <div class="home-container">
+                    <div class="home-topics">
+                        
+                        <img src="https://cdn.shopify.com/s/files/1/0229/0839/files/bancos_de_imagenes_gratis.jpg?v=1630420628" alt="Mirador Chulisimo">
+                        
+                        
+                    </div>
+                </div>
+
+                <div class="text-topics">
+                    <div>
+                        <button><i class="fa-regular fa-heart" onclick="changeIcon(this)"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-comment"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-floppy-disk"></i></button>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-share-from-square"></i></button>
+                    </div>
+                </div>
+
+                <div class="text-topics">
+                    <a href="#">Ver todos los comentarios</a>
+                </div>
+            </div>  
+
+        </div>
+    </div>
+</div>
+@endsection
+
 
 
 
