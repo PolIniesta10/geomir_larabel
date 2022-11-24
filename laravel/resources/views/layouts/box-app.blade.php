64978afff18641533ece9c72183727d878d528b9
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2022-11-24 17:19:57 +0100

    Diseñando...

--- laravel/resources/views/layouts/box-app.blade.php
+++ laravel/resources/views/layouts/box-app.blade.php
@@ -69,16 +69,3 @@
         <div class="indicator"></div>
     </ul>
 </div>
-<script>
-    let list = document.querySelectorAll('li');
-    for (let i=0; i<list.lenght; i++){
-        list[i].onmouseover = function(){
-            let j = 0;
-            while (j < list.lenght){
-                list[j++].className = 'list';
-            }
-            list[i].className = 'list active'
-        }
-    }
-
-</script>
