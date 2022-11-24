6dcb16846472ba20e9601627bc40d73cc0d01ccd
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2022-11-24 16:09:32 +0100

    Diseñando...

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/js/script.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
@@ -84,54 +84,7 @@
         </nav>
 
         <main class="fondohome">
-            <div class="navigation">
-                <ul>
-                    <li class="list active">
-                        <a href="#">
-                            <span class="icon"><i class="fa-solid fa-house"></i></span>
-                            <span class="title">Home</span>
-                        </a>
-                    </li>
-                    <li class="list">
-                        <a href="#">
-                            <span class="icon"><i class="fa-solid fa-magnifying-glass"></i></span>
-                            <span class="title">Buscar</span>
-                        </a>
-                    </li>
-                    <li class="list">
-                        <a href="#">
-                            <span class="icon"><i class="fa-solid fa-camera-retro"></i></span>
-                            <span class="title">Publicar</span>
-                        </a>
-                    </li>
-                    <li class="list">
-                        <a href="#">
-                            <span class="icon"><i class="fa-solid fa-bell"></i></span>
-                            <span class="title">Notificaciones</span>
-                        </a>
-                    </li>
-                    <li class="list">
-                        <a href="#">
-                            <span class="icon"><i class="fa-solid fa-user"></i></span>
-                            <span class="title">Perfil</span>
-                        </a>
-                    </li>
-                    <div class="indicator"></div>
-                </ul>
-            </div>
-            <script>
-                let list = document.querySelectorAll('li');
-                for (let i=0; i<list.lenght; i++){
-                    list[i].onmouseover = function(){
-                        let j = 0;
-                        while (j < list.lenght){
-                            list[j++].className = 'list';
-                        }
-                        list[i].className = 'list active'
-                    }
-                }
-
-            </script>
+            
             @include('flash')
             @yield('content')
         </main>
