9cfd31961dc45121777ddb2de020f56132490ce6
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2023-01-13 20:50:36 +0100

    m09 en el projecte

--- laravel/resources/views/about-us/about-us.blade.php
+++ laravel/resources/views/about-us/about-us.blade.php
@@ -44,38 +44,56 @@
         document.getElementById("slider").src = video;
     }
 
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
+
 </script>
 
 <div class="title-about-us">
     <h1 class="title">Conoce a nuestro equipo</h1>
 </div>
 
-<div class="container-about-us">
-    <div class="card" id="cajaPol">
-        <div class="face front">
+<div class="container-about-us" >
 
-            <img id="img-pol" src="https://album.mediaset.es/eimg/2022/06/07/ibai-revela-la-foto-original-de-su-cuenta-de-twitch_e0e3.jpg?w=1200&h=900">
-            <div class="description-about">
-                <div class="name-about"><p>Pol Iniesta</p></div>
-                <div class="linia"></div>
-                <div class="surname-about"><p>Developer</p></div>
-            </div>
-            
-        </div>
+    <div class="drag-drop" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+    <div class="drag-drop" ondrop="drop(event)" ondragover="allowDrop(event)">
 
-        <audio src="CANELITA - JUANITO JUAN (VIDEOCLIP OFICIAL) (NUEVOEXITOS.COM).mp3" id="audioPol"></audio>
+        <div class="card" id="cajaPol" draggable="true" ondragstart="drag(event)">
+            <div class="face front-pol">
 
-        <div class="face back">
-
-            <img id="img-pol" src="https://2img.net/h/2.bp.blogspot.com/_i9W6F3HrNNY/SHsADgjWdqI/AAAAAAAAA7Y/HUbMBdXbOb4/s400/cu%C3%B1ao.jpg">
-            <div class="description-about">
-                <div class="name-about"><p>Pol Iniesta</p></div>
-                <div class="linia"></div>
-                <div class="surname-about"><p>El jefe</p></div>
+                <img id="img-pol" src="https://album.mediaset.es/eimg/2022/06/07/ibai-revela-la-foto-original-de-su-cuenta-de-twitch_e0e3.jpg?w=1200&h=900" draggable="false" >
+                <div class="description-about">
+                    <div class="name-about"><p>Pol Iniesta</p></div>
+                    <div class="linia"></div>
+                    <div class="surname-about"><p>Developer</p></div>
+                </div>
+                
             </div>
 
-        </div>
+            <audio src="CANELITA - JUANITO JUAN (VIDEOCLIP OFICIAL) (NUEVOEXITOS.COM).mp3" id="audioPol"></audio>
 
+            <div class="face back-pol">
+
+                <img id="img-pol" src="https://2img.net/h/2.bp.blogspot.com/_i9W6F3HrNNY/SHsADgjWdqI/AAAAAAAAA7Y/HUbMBdXbOb4/s400/cu%C3%B1ao.jpg" draggable="false" >
+                <div class="description-about">
+                    <div class="name-about"><p>Pol Iniesta</p></div>
+                    <div class="linia"></div>
+                    <div class="surname-about"><p>El jefe</p></div>
+                </div>
+
+            </div>
+        </div>
     </div>
 
 <!-- 
@@ -83,31 +101,35 @@
                             CAJA OSCAR
     /////////////////////////////////////////////////////////////// -->
 
-    <div class="card" id="cajaOscar">
-        <div class="face front">
+    <div class="drag-drop" ondrop="drop(event)" ondragover="allowDrop(event)">
+        <div class="card" id="cajaOscar" draggable="true" ondragstart="drag(event)">
+            <div class="face front-oscar">
 
-            <img id="img-pol" src="https://2img.net/h/2.bp.blogspot.com/_i9W6F3HrNNY/SHsADgjWdqI/AAAAAAAAA7Y/HUbMBdXbOb4/s400/cu%C3%B1ao.jpg">
-            <div class="description-about">
-                <div class="name-about"><p>Oscar Mejías</p></div>
-                <div class="linia"></div>
-                <div class="surname-about"><p>Pardillo</p></div>
-            </div>
-            
-        </div>
-
-        <audio src="Bob Esponja Intro (Español de España) (NUEVOEXITOS.COM).mp3" id="audioOscar"></audio>
-
-        <div class="face back">
-
-            <img id="img-pol" src="https://album.mediaset.es/eimg/2022/06/07/ibai-revela-la-foto-original-de-su-cuenta-de-twitch_e0e3.jpg?w=1200&h=900">
-            <div class="description-about">
-                <div class="name-about"><p>Oscar Mejías</p></div>
-                <div class="linia"></div>
-                <div class="surname-about"><p>Bob Esponja</p></div>
+                <img id="img-oscar" src="https://2img.net/h/2.bp.blogspot.com/_i9W6F3HrNNY/SHsADgjWdqI/AAAAAAAAA7Y/HUbMBdXbOb4/s400/cu%C3%B1ao.jpg" draggable="false" >
+                <div class="description-about">
+                    <div class="name-about"><p>Oscar Mejías</p></div>
+                    <div class="linia"></div>
+                    <div class="surname-about"><p>Pardillo</p></div>
+                </div>
+                
             </div>
 
+            <audio src="Bob Esponja Intro (Español de España) (NUEVOEXITOS.COM).mp3" id="audioOscar"></audio>
+
+            <div class="face back-oscar">
+
+                <img id="img-oscar" src="https://album.mediaset.es/eimg/2022/06/07/ibai-revela-la-foto-original-de-su-cuenta-de-twitch_e0e3.jpg?w=1200&h=900" draggable="false">
+                <div class="description-about">
+                    <div class="name-about"><p>Oscar Mejías</p></div>
+                    <div class="linia"></div>
+                    <div class="surname-about"><p>Bob Esponja</p></div>
+                </div>
+
+            </div>
         </div>
     </div>
+    <div class="drag-drop" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+
 </div>
 
 <!-- 
@@ -119,17 +141,20 @@
 <div class="modal">
     <div class="modal-content">
         <div class="modal-header">
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button type="button" class="close btn btn-primary" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
         <div class="modal-body"></div>
             <video id="slider" autoplay loop>
                 <source src="CrazyFrog.mp4" type="video/mp4">
             </video>
             <ul class="menu-navigation">
-                <li onclick="videoURl('CrazyFrog.mp4')"><img src="CrazyFrog.jpg"></li>
-                <li onclick="videoURl('OsitoGominola.mp4')"><img src="OsitoGominola.jpg"></li>
+                <li onclick="videoURl('./videos/CrazyFrog.mp4')"><img src="CrazyFrog.jpg"></li>
+                <li onclick="videoURl('./videos/OsitoGominola.mp4')"><img src="OsitoGominola.jpg"></li>
             </ul>
         </div>
     </div>
 </div>
+
 @endsection
