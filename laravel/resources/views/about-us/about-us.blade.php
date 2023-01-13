72d819926adf8dd01def92dd4d167cf89ea2b2c3
osmepa <osmepa@fp.insjoaquimmir.cat>
2023-01-13 21:07:30 +0100

    intento de arreglar modal

--- laravel/resources/views/about-us/about-us.blade.php
+++ laravel/resources/views/about-us/about-us.blade.php
@@ -59,7 +59,7 @@
     }
 
 </script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script> 
 <div class="title-about-us">
     <h1 class="title">Conoce a nuestro equipo</h1>
 </div>
